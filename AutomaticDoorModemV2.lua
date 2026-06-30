aca9d070724d9eb19cb71133ee538ad972bd92b1
Hooman <hoomanperpon@gmail.com>
2026-06-30 09:22:24 -0400

    Update AutomaticDoorModemV2.lua

--- AutomaticDoorModemV2.lua
+++ AutomaticDoorModemV2.lua
@@ -1,13 +1,21 @@
 local modem = peripheral.wrap("right")
 
-local doorId = os.getComputerID()
-
 modem.open(4504)
 
-local cooldown = 1.5
+local x,y,z = gps.locate(2)
+
+if not x then
+    error("No GPS signal")
+end
+
+local coords = vector.new(x,y,z)
+
+print("Position:", coords)
+
 local timer
 
 while true do
+
     local event, side,
           channel,
           reply,
@@ -15,38 +23,53 @@ while true do
         os.pullEvent()
 
     if event == "modem_message"
-    and channel == 4504 then
-        if msg.type == "discover" then
+    and channel == 4504
+    and reply == 4505 then
+
+        if msg == "getpos" then
+
             modem.transmit(
                 4504,
                 4505,
                 {
-                    type="door",
-                    id=doorId
+                    x=x,
+                    y=y,
+                    z=z,
+                    id=os.getComputerID()
                 }
             )
-        elseif msg.type == "open"
-        and msg.id == doorId then
-            redstone.setOutput(
-                "top",
-                true
-            )
-            if timer then
-                os.cancelTimer(
-                    timer
+
+        elseif type(msg)=="table"
+        and msg.type=="open" then
+
+            if msg.id ==
+               os.getComputerID() then
+
+                redstone.setOutput(
+                    "top",
+                    true
                 )
+
+                if timer then
+                    os.cancelTimer(
+                        timer
+                    )
+                end
+
+                timer =
+                    os.startTimer(
+                        1.5
+                    )
             end
-            timer =
-                os.startTimer(
-                    cooldown
-                )
         end
 
     elseif event=="timer" then
+
         redstone.setOutput(
             "top",
             false
         )
+
         timer=nil
     end
 end
